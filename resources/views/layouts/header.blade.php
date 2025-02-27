768e5b7480d2d3d481ea4568d9451021bef723cb
Rhezatama Dwi <rhezatama.dwi@gmail.com>
2025-02-27 19:40:59 +0700

    Update border color for laporan creation link for improved visibility

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -14,7 +14,7 @@
                 @auth
                     <a
                         href="{{ route('laporan.create') }}"
-                        class="flex inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
+                        class="flex inline-block px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:border-[#007dd9] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#007dd9] rounded-sm text-sm leading-normal"
                     >
                         <x-lucide-plus class="w-5 h-5 mr-1" /> Laporan Harian
                     </a>
