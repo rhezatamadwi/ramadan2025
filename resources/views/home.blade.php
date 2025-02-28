3169b20a1f82b53927f845069e8fede8f1cbfa0d
Rhezatama Dwi <rhezatama.dwi@gmail.com>
2025-02-28 21:24:25 +0700

    Add dark mode breathing animations for schedule indicators in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,7 +20,7 @@
             @foreach ($list_jadwal_sholat as $jadwal)
                 <li class="flex items-center gap-4 py-2 relative before:border-l before:border-[#e3e3e0] dark:before:border-[#3E3E3A] before:top-1/2 before:bottom-0 before:left-[0.4rem] before:absolute">
                     <span class="relative py-1 bg-white dark:bg-[#161615]">
-                        <span class="flex items-center justify-center rounded-full bg-[#FDFDFC] dark:bg-[#161615] shadow-[0px_0px_1px_0px_rgba(0,0,0,0.03),0px_1px_2px_0px_rgba(0,0,0,0.06)] w-3.5 h-3.5 border @if($jadwal['is']) border-[#007dd9] breathing-border @else dark:border-[#3E3E3A] border-[#e3e3e0] @endif">
+                        <span class="flex items-center justify-center rounded-full bg-[#FDFDFC] dark:bg-[#161615] shadow-[0px_0px_1px_0px_rgba(0,0,0,0.03),0px_1px_2px_0px_rgba(0,0,0,0.06)] w-3.5 h-3.5 border @if($jadwal['is'])border-[#007dd9] breathing-border @else dark:border-[#3E3E3A] border-[#e3e3e0] @endif">
                             <span class="rounded-full @if($jadwal['is']) bg-[#007dd9] breathing-dot @else bg-[#dbdbd7] dark:bg-[#3E3E3A] @endif w-1.5 h-1.5"></span>
                         </span>
                     </span>
